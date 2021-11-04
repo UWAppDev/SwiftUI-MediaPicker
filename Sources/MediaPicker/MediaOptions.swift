899e6c148bdb00d95cc2a41bfbe5a23ec25e944f
Apollo Zhu <public-apollonian@outlook.com>
2021-11-04 15:54:07 -0700

    Remove PhotoPicker reference

--- Sources/MediaPicker/MediaOptions.swift
+++ Sources/MediaPicker/MediaOptions.swift
@@ -2,13 +2,13 @@
 //
 // This source file is part of the SwiftUI FoodTracker tutorial series
 //
-// Copyright (c) 2020-2021 AppDev@UW.edu and the SwiftUI PhotoPicker authors
+// Copyright (c) 2020-2021 AppDev@UW.edu and the SwiftUI MediaPicker authors
 // Licensed under MIT License
 //
-// See https://github.com/UWAppDev/SwiftUI-PhotoPicker/blob/main/LICENSE
+// See https://github.com/UWAppDev/SwiftUI-MediaPicker/blob/main/LICENSE
 // for license information
-// See https://github.com/UWAppDev/SwiftUI-PhotoPicker/graphs/contributors
-// for the list of SwiftUI PhotoPicker project authors
+// See https://github.com/UWAppDev/SwiftUI-MediaPicker/graphs/contributors
+// for the list of SwiftUI MediaPicker project authors
 //
 //===----------------------------------------------------------------------===//
 
