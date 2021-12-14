8b3ec6dd7a58eab13a8902d9011bd12cfb24435a
Apollo Zhu <public-apollonian@outlook.com>
2021-12-13 17:26:06 -0800

    Add normal xcodeproj Demo

---
+++ Demo/Shared/ContentView.swift
@@ -0,0 +1,81 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftUI FoodTracker tutorial series
+//
+// Copyright (c) 2020-2021 AppDev@UW.edu and the SwiftUI MediaPicker authors
+// Licensed under MIT License
+//
+// See https://github.com/UWAppDev/SwiftUI-MediaPicker/blob/main/LICENSE
+// for license information
+// See https://github.com/UWAppDev/SwiftUI-MediaPicker/graphs/contributors
+// for the list of SwiftUI MediaPicker project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftUI
+import AVKit
+import MediaPicker
+
+struct ContentView: View {
+    @State var urls: [URL] = []
+    @State var isShowingMediaPicker = false
+    
+    var selectButton: some View {
+        Button("Select Media") {
+            isShowingMediaPicker = true
+        }
+        .mediaImporter(isPresented: $isShowingMediaPicker,
+                       allowedMediaTypes: .all,
+                       allowsMultipleSelection: true) { result in
+            switch result {
+            case .success(let urls):
+                self.urls = urls
+            case .failure(let error):
+                print(error)
+                self.urls = []
+            }
+        }
+    }
+    
+    var body: some View {
+        List {
+            Section {
+                ForEach(urls, id: \.absoluteString) { url in
+                    switch try! url.resourceValues(forKeys: [.contentTypeKey]).contentType! {
+                    case let contentType where contentType.conforms(to: .image):
+                        if #available(iOS 15, macOS 12, *) {
+                            AsyncImage(url: url) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                        } else {
+                            if let image = NativeImage(contentsOfFile: url.path) {
+                                Image(nativeImage: image)
+                                    .resizable()
+                                    .scaledToFit()
+                            } else {
+                                Text("Can't load contents of \(url)")
+                            }
+                        }
+                    case let contentType where contentType.conforms(to: .audiovisualContent):
+                        VideoPlayer(player: AVPlayer(url: url))
+                            .scaledToFit()
+                    default:
+                        Text("Can't display \(url.lastPathComponent)")
+                    }
+                }
+            } header: {
+                selectButton
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo/Shared/NativeImage.swift
@@ -0,0 +1,37 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftUI FoodTracker tutorial series
+//
+// Copyright (c) 2020-2021 AppDev@UW.edu and the SwiftUI MediaPicker authors
+// Licensed under MIT License
+//
+// See https://github.com/UWAppDev/SwiftUI-MediaPicker/blob/main/LICENSE
+// for license information
+// See https://github.com/UWAppDev/SwiftUI-MediaPicker/graphs/contributors
+// for the list of SwiftUI MediaPicker project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftUI
+
+#if canImport(UIKit)
+import UIKit
+
+typealias NativeImage = UIImage
+
+extension Image {
+    init(nativeImage: NativeImage) {
+        self.init(uiImage: nativeImage)
+    }
+}
+#else
+import AppKit
+
+typealias NativeImage = NSImage
+
+extension Image {
+    init(nativeImage: NativeImage) {
+        self.init(nsImage: nativeImage)
+    }
+}
+#endif
